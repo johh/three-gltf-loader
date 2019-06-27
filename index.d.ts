13a283262d39081dec7f0e13df3f6a9bf0938050
JohH <code@johh.net>
2019-06-27 23:09:19 +0200

    TASK: update to v0.106.0

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
 declare module 'three-gltf-loader' {
 	import { AnimationClip, Camera, LoadingManager, Scene } from 'three';
+	import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+	import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
 
 	interface GLTF {
 		animations: AnimationClip[];
@@ -13,13 +15,18 @@ declare module 'three-gltf-loader' {
 
 		constructor(manager?: LoadingManager);
 		manager: LoadingManager;
+		dracoLoader: DRACOLoader | null;
+		ddsLoader: DDSLoader | null;
 		path: string;
+		crossOrigin: string;
+		resourcePath: string;
 
 		load(url: string, onLoad: (gltf: GLTF) => void, onProgress?: (event: ProgressEvent) => void, onError?: (event: ErrorEvent) => void): void;
 		setPath(path: string): GLTFLoader;
 		setResourcePath(path: string): GLTFLoader;
 		setCrossOrigin(value: string): GLTFLoader;
-		setDRACOLoader(dracoLoader: object): GLTFLoader;
+		setDRACOLoader(dracoLoader: DRACOLoader): GLTFLoader;
+		setDDSLoader(ddsLoader: DDSLoader): GLTFLoader;
 		parse(data: ArrayBuffer | string, path: string, onLoad: (gltf: GLTF) => void, onError?: (event: ErrorEvent) => void): void;
 
 	}
